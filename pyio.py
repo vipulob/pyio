94817f8cc320a82ead3d6bb5c2345eb817b90b58
Vipul Borikar <vipucell@gmail.com>
2018-01-28 15:01:28 +0530

    Issue: The program needs a input.
    Solution:
    Used sys.argv in the program to accept data.

--- pyio.py
+++ pyio.py
@@ -1,7 +1,11 @@
+import sys
+
 import libpyio
+
 
 if __name__== "__main__":
     print("Starting the PYIO program");
-    
+    print("The number of arguments received: %d" %len(sys.argv))
+
     # Call init function
     libpyio.Initialize()
